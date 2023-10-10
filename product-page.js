b1cbc9ca08410e043548df67086931e9331e2c29
ahmyk9 <ahmedkhilji99@gmail.com>
2023-10-10 20:34:27 +1100

    added nfc reading

--- product-page.js
+++ product-page.js
@@ -8,7 +8,7 @@ const displayItem = await fetch(
     console.log(displayItemData);
     console.log(window.location)
     
-    async function itemFetcher() {
+async function itemFetcher() {
   const ItemElement = document.querySelector(".item");
 
   const itemName = ItemElement.querySelector("#item__Name");
@@ -52,3 +52,32 @@ window.addEventListener("popstate", handleURLChange);
 handleURLChange();
 
 
+// NFC Scanning
+
+const scan = async() =>{
+    var paragraph = document.getElementById("myParagraph");
+    if ('NDEFReader' in window) { 
+        try {
+            const ndef = new window.NDEFReader();
+            await ndef.scan();
+            
+            console.log("Scan started successfully.");
+            ndef.onreadingerror = () => {
+                console.log("Cannot read data from the NFC tag. Try another one?");
+            };
+            
+            ndef.onreading = event => {
+                console.log("NDEF message read.");
+                paragraph.innerHTML = "This is the new content.";
+                onReading(event);
+                setActions({
+                    scan: 'scanned',
+                    write: null
+                });
+            };
+
+        } catch(error){
+            console.log(`Error! Scan failed to start: ${error}.`);
+        };
+    }
+};
